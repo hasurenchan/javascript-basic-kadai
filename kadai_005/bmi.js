0ec139ba87f98773ecd5381d7d6b1a9053f5aab4
hasurenchan <80143491+hasurenchan@users.noreply.github.com>
2024-05-12 09:57:21 +0900

    Delete kadai_005 directory

--- kadai_005/bmi.js
+++
@@ -1,8 +0,0 @@
-// 体重（kg）
-const weight = 68;
-
-// 身長（m）
-const height = 1.7;
-
-// BMIは、体重 ÷ 身長 × 身長 で求められる。
-console.log(weight / (height * height));
--- kadai_005/kadai_005/bmi.js
+++
@@ -1,8 +0,0 @@
-// 体重（kg）
-let weight = 68;
-
-// 身長（m）
-let height = 1.7;
-
-// BMIは、体重 ÷ 身長 × 身長 で求められる。
-console.log(weight / (height * height));
